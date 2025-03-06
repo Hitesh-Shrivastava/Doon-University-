a30f40f8dc9884b181d6687f8babb566eab120ea
Hitesh Shrivastava <140699472+Hitesh-Shrivastava@users.noreply.github.com>
2025-03-06 15:33:22 +0530

    AddNumbers.java

---
+++ AddNumbers.java
@@ -0,0 +1,7 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, sum;
+        sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
